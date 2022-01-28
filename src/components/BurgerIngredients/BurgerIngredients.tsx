5ac902005bd11232393e959d69a069d6f7c3d9be
valery-ish <valery-ish@yandex.ru>
2022-01-28 18:30:28 +0300

    propTypes

--- src/components/BurgerIngredients/BurgerIngredients.tsx
+++ src/components/BurgerIngredients/BurgerIngredients.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import {useState} from 'react';
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
 import burgerIngredientsStyles from './BurgerIngredients.module.css';
 import { Tab, CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components'
-import { dataImport } from '../../utils/data.js'
+import { data } from '../../utils/data.js'
+import { render } from '@testing-library/react';
 
 function TabSet() {
-  const [current, setCurrent] = React.useState('one')
-    return (
-      <div style={{ display: 'flex' }}>
-        <Tab value="one" active={current === 'one'} onClick={setCurrent}>
-          One
-        </Tab>
-        <Tab value="two" active={current === 'two'} onClick={setCurrent}>
-          Two
-        </Tab>
-        <Tab value="three" active={current === 'three'} onClick={setCurrent}>
-          Three
-        </Tab>
-      </div>
-    ) 
+  const [current, setCurrent] = useState('one')
+  return (
+    <div style={{ display: 'flex' }}>
+      <Tab value="one" active={current === 'one'} onClick={setCurrent}>
+        One
+      </Tab>
+      <Tab value="two" active={current === 'two'} onClick={setCurrent}>
+        Two
+      </Tab>
+      <Tab value="three" active={current === 'three'} onClick={setCurrent}>
+        Three
+      </Tab>
+    </div>
+  )
 }
 
 const Card = ({card}) => (
@@ -33,59 +34,44 @@ const Card = ({card}) => (
       </div>
 )
 
-const BunCards = ({dataImport}) => (
+Card.propTypes = {
+  card: PropTypes.object,
+  name: PropTypes.string,
+  price: PropTypes.number,
+  image: PropTypes.string
+}
+
+const TypeCards = ({data, prop}) => (
   <div className={burgerIngredientsStyles.cards + ' ml-4 mr-2'}>
-    {dataImport.map(data => {
-      if (data.type === 'bun') {
-        return <Card card={data}/> 
+    {data.map(dataElement => {
+      if (dataElement.type === prop) {
+        return <Card card={dataElement} key={dataElement._id}/>
       }      
     })}
   </div> 
 )
 
-const MainCards = ({dataImport}) => (
-  <div className={burgerIngredientsStyles.cards + ' ml-4 mr-2'}>
-    {dataImport.map(data => {
-      if (data.type === 'main') {
-        return <Card card={data}/> 
-      }      
-    })}
-  </div> 
-)
+TypeCards.propTypes = {
+  dataElement: PropTypes.object,
+  _id: PropTypes.string
+}
 
-const SauceCards = ({dataImport}) => (
-  <div className={burgerIngredientsStyles.cards + ' ml-4 mr-2'}>
-    {dataImport.map(data => {
-      if (data.type === 'sauce') {
-        return <Card card={data}/> 
-      }      
-    })}
-  </div> 
-)
-
-class BurgerIngredients extends React.Component {
-  state = { dataImport}
-  render() {    
-    return (
-      <section className={burgerIngredientsStyles.section + ' mr-10'}>
-        <h1 className='text text_type_main-large mt-10 mb-5'>Соберите бургер</h1>
-
-        {/* <TabSet/> */}
-
-        <div className={burgerIngredientsStyles.scrollSection}>
-          <h2 className='text text_type_main-medium mb-6'>Булки</h2>
-          <BunCards dataImport={this.state.dataImport}/>        
-
-          <h2 className='text text_type_main-medium mb-6 mt-10'>Соусы</h2>
-          <SauceCards dataImport={this.state.dataImport}/>
-
-          <h2 className='text text_type_main-medium mb-6 mt-10'>Начинки</h2>
-          <MainCards dataImport={this.state.dataImport}/>
-        </div>
-        
-      </section>
-    );
-  }
+function BurgerIngredients() {    
+  return (
+    <section className={burgerIngredientsStyles.section + ' mr-10'}>
+      <h1 className='text text_type_main-large mt-10 mb-5'>Соберите бургер</h1>
+     <TabSet/>
+     <div className={burgerIngredientsStyles.scrollSection}>
+        <h2 className='text text_type_main-medium mb-6'>Булки</h2>
+        <TypeCards data={data} prop='bun'/>        
+       <h2 className='text text_type_main-medium mb-6 mt-10'>Соусы</h2>
+        <TypeCards data={data} prop='main'/>
+       <h2 className='text text_type_main-medium mb-6 mt-10'>Начинки</h2>
+        <TypeCards data={data} prop='sauce'/>
+      </div>
+      
+    </section>
+  );  
 }
 
 export default BurgerIngredients;
